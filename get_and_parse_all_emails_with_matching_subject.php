326cfe6ac0ab53153d12140b55d004a701249757
Kelvin Quee <kelvin@quee.org>
2021-04-20 19:46:33 +0800

    Added checks for versions, ability to get email subject and body, and thereafter write to Dokuwiki data directory at specified namespace.
    TODO
    Nested namespace support
    Attachments
    How to handle multiple posted emails

--- get_and_parse_all_emails_with_matching_subject.php
+++ get_and_parse_all_emails_with_matching_subject.php
@@ -7,21 +7,31 @@
      */
     declare(strict_types=1);
 
-    require_once __DIR__.'/../vendor/autoload.php';
+    require_once __DIR__.'/vendor/autoload.php';
+    $path_to_doku = '../../';
+    $namespace = 'personal';
+
+	// Check path to Dokuwiki and version is correct
+	if (file_exists($path_to_doku.'VERSION')) {
+  			$print_version = file_get_contents($path_to_doku.'VERSION');
+  			echo $print_version;
+		} else {
+			exit('File VERSION does not exist. Please check Dokuwiki path is correct');
+		}
 
     use PhpImap\Exceptions\ConnectionException;
     use PhpImap\Mailbox;
 
     $mailbox = new Mailbox(
-        '{imap.gmail.com:993/imap/ssl}INBOX', // IMAP server and mailbox folder
-        'some@gmail.com', // Username for the before configured mailbox
-        '*********', // Password for the before configured username
+        '{imap.migadu.com:993/imap/ssl}INBOX', // IMAP server and mailbox folder
+        'post-to-wiki@quee.org', // Username for the before configured mailbox
+        'Gills0-Reward-Carport-Islamist-Glitzy', // Password for the before configured username
         __DIR__, // Directory, where attachments will be saved (optional)
         'US-ASCII' // Server encoding (optional)
     );
 
     try {
-        $mail_ids = $mailbox->searchMailbox('SUBJECT "part of the subject"');
+        $mail_ids = $mailbox->searchMailbox('SUBJECT "[9d8uu]"');
     } catch (ConnectionException $ex) {
         die('IMAP connection failed: '.$ex->getMessage());
     } catch (Exception $ex) {
@@ -52,11 +62,32 @@
         if (!empty($email->getAttachments())) {
             echo \count($email->getAttachments())." attachements\n";
         }
-        if ($email->textHtml) {
+/*        if ($email->textHtml) {
             echo "Message HTML:\n".$email->textHtml;
         } else {
             echo "Message Plain:\n".$email->textPlain;
-        }
+        } */
+        
+        echo "Message Plain:\n".$email->textPlain;
+        
+        //Write to Dokuwiki
+        
+        $pagename = preg_replace('/[[:space:]]+/', '-', trim(explode(']',(string) $email->subject)[1])); //Create wikipage name
+        $target_page = $path_to_doku.'data/pages/'.$namespace.'/'.$pagename.'.txt';
+        echo 'writing to target_page'.$target_page; //Begin writing
+
+        
+        if (file_exists($target_page)) {
+        	exit('Error: This wiki page already exists.');
+		} else {
+        	$fp = fopen($target_page, 'w+') or exit('Error: Cannot open file to create wiki page.');
+			echo 'writing to target_page'.$target_page; //Begin writing
+			fwrite($fp, $email->textPlain) or die('ERROR: Cannot write to configuration file.');
+        	flock($fp, LOCK_UN) or die ('ERROR: Cannot unlock file');
+			fclose($fp);
+			echo 'Wiki page successfully created, written, and closed.'; //Write success
+		}
+
 
         if (!empty($email->autoSubmitted)) {
             // Mark email as "read" / "seen"
