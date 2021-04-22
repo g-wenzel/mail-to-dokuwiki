b6ae76a97e57346fc368fca40f0488941111c14a
Kelvin Quee <kelvin@quee.org>
2021-04-22 22:21:26 +0800

    Added feature to send email containing link to newly posted wiki page.

--- post-to-wiki.php
+++ post-to-wiki.php
@@ -3,37 +3,46 @@
     /**
      * Post to Dokuwiki
      *
-     * Pulls out email with specified prefix (eg [XYZ123]) and post it to your Dokuwiki.
-     * Largely based upon example work of @author Sebastian Krätzig <info@ts3-tools.info>
+     * Pulls out email with specified prefix (eg [secret_word]) and post it to your Dokuwiki.
+     * Largely based upon example work of Sebastian Krätzig <info@ts3-tools.info> (PHPIMAP) and PHPMailer.
+     *
+     * As this was supposed to be a Dokuwiki plugin but I have yet to figure out how to make this work, please still include this file within /lib/plugins/post-to-wiki.
+     *
+     * TODO
+     * Sometimes Dokuwiki's search does not find the newly created files. You may need to run php bin/indexer.php manually.
+     * Supports only first level namespaces only. To support deeper namespaces.
      *
      * @author Kelvin Quee <kelvin@quee.org>
      */
 
     declare(strict_types=1);
+    require_once __DIR__.'/vendor/autoload.php';
 
+    use PhpImap\Exceptions\ConnectionException;
+    use PhpImap\Mailbox;
+    use PHPMailer\PHPMailer\PHPMailer;
+    use PHPMailer\PHPMailer\SMTP;
     // Load configuration and credentials from .env files
     $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
     $dotenv->load();
 
     $path_to_doku = $_ENV['path_to_doku'];	// Relative path to Dokuwiki root. TODO - Convert into plugin.
-    $namespace = $_ENV['namespace'];	// Namespace to create wiki pages in. TODO - Support deeper levels of namespaces.
+    $namespace = $_ENV['namespace'];	// Namespace to create wiki pages in. First level only.
     $target_mailbox = $_ENV['target_mailbox'];
     $target_mail_subject_prefix = $_ENV['target_mail_subject_prefix']; // Only mails with subject line beginning with this will be retrieved and created into wiki pages.
     $mail_username = $_ENV['mail_username'];
-    $mail_password = $_ENV['mail_password'];
+    $mail_password = $_ENV['mail_password'];    
 
-    require_once __DIR__.'/vendor/autoload.php';	// Path to composer.
-
-	// Check path to Dokuwiki and version is correct. TODO
+	// Check path to Dokuwiki and version is the latest stable (2020-07-29 "Hogfather").
 	if (file_exists($path_to_doku.'VERSION')) {
   			$print_version = file_get_contents($path_to_doku.'VERSION');
   			echo $print_version;
+            if (str_contains ($print_version, "Hogfather")) {
+                echo ("is tested Dokuwiki version.\n");
+            } else { 
+                exit('Version of Dokuwiki is not as expected. You may disable this warning and proceed with caution.');}
 		} else {
-			exit('File VERSION does not exist. Please check Dokuwiki path is correct');
-		}
-
-    use PhpImap\Exceptions\ConnectionException;
-    use PhpImap\Mailbox;
+			exit('File VERSION does not exist. Please check Dokuwiki path is correct');}
 
     function sanitize_filename($target_string) {
         return preg_replace('/[^a-z0-9]+/', '-', strtolower($target_string));
@@ -43,12 +52,10 @@
         $target_mailbox,
         $mail_username,
         $mail_password,
-        __DIR__, // Directory, where attachments will be saved (optional)
-        'US-ASCII' // Server encoding (optional)
     );
 
     try {
-        $mail_ids = $mailbox->searchMailbox('SUBJECT "[9d8uu]" UNSEEN'); // Find all mails with matching subject and not read
+        $mail_ids = $mailbox->searchMailbox('SUBJECT '.$target_mail_subject_prefix.' UNSEEN'); // Find all mails with matching subject and not read
     } catch (ConnectionException $ex) {
         die('IMAP connection failed: '.$ex->getMessage());
     } catch (Exception $ex) {
@@ -56,34 +63,26 @@
     }
 
     foreach ($mail_ids as $mail_id) {
-        echo "+------ P A R S I N G ------+\n";
-
         $email = $mailbox->getMail(
             $mail_id, // ID of the email, you want to get
             true // Mark retrieved emails as read
         );
 
-        echo 'Marking '.count($mail_ids).' emails as read.\n';
-
-        echo 'from-name: '.(string) (isset($email->fromName) ? $email->fromName : $email->fromAddress)."\n";
-        echo 'from-email: '.(string) $email->fromAddress."\n";
-        echo 'to: '.(string) $email->toString."\n";
-        echo 'subject: '.(string) $email->subject."\n";
-        echo 'message_id: '.(string) $email->messageId."\n";
+        echo "Marking ".count($mail_ids)." matching emails as read.\n";
 
         //Create wikipage name using multiple operations to make it Dokuwiki-ish.
         $pagename_wip = strtolower(preg_replace('/[[:space:]]+/', '-', trim(implode((array_slice(explode(']',(string) $email->subject), 1)),"]")))); 
         $pagename = sanitize_filename($pagename_wip);
-        $target_page = $path_to_doku.'data/pages/'.$namespace.'/'.$pagename.'.txt'; // Future - Support deeper namespaces
+        $target_page = $path_to_doku.'data/pages/'.$namespace.'/'.$pagename.'.txt'; 
 
         $attachments = $email->getAttachments();
-        $wikipage_content = $email->textPlain; // We take text only from plain text for now. Future - HTML support.
+        $wikipage_content = $email->textPlain; // We take text only from plain text for now. TODO - HTML support.
 
         foreach ($attachments as $attachment) {
 
             // Some string gymnastics to create sane attachments filenames. To be improved.
             $ext = pathinfo($attachment->name)['extension'];
-            $target_attachment_filename = time().sanitize_filename(pathinfo($attachment->name)['filename']).".".$ext;;
+            $target_attachment_filename = time()."-".sanitize_filename(pathinfo($attachment->name)['filename']).".".$ext;;
             $target_attachment_filepath = $path_to_doku.'data/media/'.$namespace.'/'.$target_attachment_filename;
 
             $attachment->setFilePath($target_attachment_filepath);
@@ -92,21 +91,40 @@
             // Add attachment Dokuwiki markup to wiki content
             $wikipage_content .= "\n{{ :".$namespace.":".$target_attachment_filename." |}}";
         }
-        
-        //Write to Dokuwiki
-        
+                
         if (file_exists($target_page)) {
         	echo("Error: This wiki page already exists.\n");
 		} 
         else {
+            // Write text files (create new page) in Dokuwiki
             file_put_contents($target_page, $wikipage_content, FILE_APPEND | LOCK_EX);
+
+            // Send email containing direct link to new wiki page using PHPMailer. Your requiresment may differ and as such please debug using - https://github.com/PHPMailer/PHPMailer/wiki/Troubleshooting
+
+            try {
+                $mail = new PHPMailer();
+                $mail->isSMTP();
+                $mail->SMTPDebug = SMTP::DEBUG_OFF;
+                $mail->Host = $_ENV['smtp_server'];
+                $mail->Port = $_ENV['smtp_port'];
+                $mail->SMTPAuth = true;
+                $mail->Username = $_ENV['smtp_username'];
+                $mail->Password = $_ENV['smtp_password'];
+                $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS; // You may need PHPMailer::ENCRYPTION_STARTTLS. In that case, your SMTP port may be 587.
+                $mail->AuthType = 'PLAIN';
+
+                $mail->Subject = $pagename." has been posted to ".$_ENV['your_dokuwiki_url'];
+                $mail->Body = "Your created wiki page at ".$_ENV['your_dokuwiki_url']."/".$namespace."/".$pagename;           
+                
+                $mail->setFrom($_ENV['mail_username'], 'Post-to-Dokuwiki');
+                $mail->addAddress($email->fromAddress);
+                $mail->send();
+                echo "Email for ".$pagename." successfully sent.\n";
+            }
+            catch (Exception $e) {
+                echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}\n";
+            }
 		}
-
-        // TODO
-        // Sometimes Dokuwiki's search does not find the newly created files. To run php bin/indexer.php manually.
-        // Workaround is to email hyperlinks to newly created pages link to the requester.
-
     }
-
     $mailbox->disconnect();
 
